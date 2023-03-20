0723288f29f8c403f3fb274cda103afbb14ecbc6
ausias <ausias@hINFO6-8.xiam.cat>
2023-03-20 11:56:55 +0100

    pol

--- Uf4-Practica6/src/uf4/practica6/Uf4Practica6.java
+++ Uf4-Practica6/src/uf4/practica6/Uf4Practica6.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package uf4.practica6;
 
-/**
- *
- * @author ausias
- */
+import java.util.*;
+
 public class Uf4Practica6 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
